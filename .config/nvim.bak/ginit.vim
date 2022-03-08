5474ba4277fddd7bcd57757ec0729666c14b3a14
Shantanu Shukla <shantanu.shukla10@gmail.com>
2022-03-09 00:29:36 +0530

    switched to neovim lua based on AstroVim

--- .config/nvim.bak/ginit.vim
+++ .config/nvim.bak/ginit.vim
@@ -1,6 +1,6 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""" " Neovim-Qt Settings " """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
-GuiFont CaskaydiaCove Nerd Font:h10
+GuiFont Cascadia Code:h10
 GuiTabline 0
 GuiPopupmenu 0
 
---
+++ .config/nvim/lua/user/settings.lua
@@ -0,0 +1,76 @@
+local config = {
+
+  -- Set colorscheme
+  colorscheme = "onedark",
+
+  -- Add plugins
+  plugins = {
+    { "lambdalisue/suda.vim" },
+    -- { "andweeb/presence.nvim" },
+    -- {
+    -- "ray-x/lsp_signature.nvim",
+    -- event = "BufRead",
+    -- config = function()
+    -- require("lsp_signature").setup()
+    -- end,
+    -- },
+  },
+
+  overrides = {
+    treesitter = {
+      ensure_installed = { "lua" },
+      ensure_installed = { "c" },
+      ensure_installed = { "rust" },
+      ensure_installed = { "java" },
+      ensure_installed = { "vim" },
+    },
+  },
+
+  -- On/off virtual diagnostics text
+  virtual_text = true,
+
+  -- Disable default plugins
+  enabled = {
+    bufferline = true,
+    nvim_tree = true,
+    lualine = true,
+    lspsaga = true,
+    gitsigns = true,
+    colorizer = true,
+    toggle_term = true,
+    comment = true,
+    symbols_outline = true,
+    indent_blankline = true,
+    dashboard = true,
+    which_key = true,
+    neoscroll = true,
+    ts_rainbow = true,
+    ts_autotag = true,
+  },
+
+  packer_file = vim.fn.stdpath "config" .. "/lua/packer_compiled.lua",
+
+  polish = function()
+    local opts = { noremap = true, silent = true }
+    local map = vim.api.nvim_set_keymap
+    local set = vim.opt
+    -- Set options
+    set.relativenumber = true
+
+    -- Set key bindings
+    map("n", "<C-s>", ":w!<CR>", opts)
+
+    -- Set autocommands
+    vim.cmd [[
+      augroup packer_conf
+        autocmd!
+        autocmd bufwritepost plugins.lua source <afile> | PackerSync
+      augroup end
+    ]]
+  end,
+}
+
+-- vim suda plugin settings
+vim.g["suda_smart_edit"] = 1
+
+return config
