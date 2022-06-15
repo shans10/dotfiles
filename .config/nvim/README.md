a2abfdb69c1b157414f0ebe4bd4fdb856f7508bc
Shantanu Shukla <shantanu.shukla10@gmail.com>
2022-06-15 14:59:12 +0530

    moved Neovim config

--- .config/nvim/README.md
+++ .config/nvim/README.md
@@ -1,15 +1,3 @@
 <h1 align="center">Doom Neovim</h1>
 
-**This is my personal configuration of [AstroNvim](https://github.com/AstroNvim/AstroNvim) with some additional keybindings and aesthetic changes.**
-Visit their official gihub page for support and configuration.
-
-<h2 align="centre">Usage</h2>
-
- - To use this config paste the nvim folder in `~/.config/` in Linux or
-   `%LOCALAPPDATA%` on Windows.
-
- - After that run `nvim +PackerSync` in terminal to install all plugins.
-
-**NOTE:** For adding personal configuration check user_example/init.lua and paste it in user/init.lua.
-
-**Pulled changes upto commit 00bb334b6b7e797792724488dfcd0743c3962dce from AstroNvim.**
+**Find my personal Neovim config at [Doom Neovim](https://github.com/shans10/DoomNvim) forked from [AstroNvim](https://github.com/AstroNvim/AstroNvim) .**
--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -1,8 +1,5 @@
 local config = {
 
-  -- Set colorscheme
-  colorscheme = "default_theme",
-
   -- Configure plugins
   plugins = {
     -- Add plugins, the packer syntax without the "use"
@@ -32,44 +29,6 @@ local config = {
     virtual_text = true,
     underline = false,
   },
-
-  -- null-ls configuration
-  ["null-ls"] = function()
-    -- Formatting and linting
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim
-    local status_ok, null_ls = pcall(require, "null-ls")
-    if not status_ok then
-      return
-    end
-
-    -- Check supported formatters
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    local formatting = null_ls.builtins.formatting
-
-    -- Check supported linters
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    local diagnostics = null_ls.builtins.diagnostics
-
-    null_ls.setup {
-      debug = false,
-      sources = {
-        -- Set a formatter
-        formatting.rufo,
-        -- Set a linter
-        diagnostics.rubocop,
-      },
-      -- NOTE: You can remove this on attach function to disable format on save
-      on_attach = function(client)
-        if client.resolved_capabilities.document_formatting then
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            desc = "Auto format before save",
-            pattern = "<buffer>",
-            callback = vim.lsp.buf.formatting_sync,
-          })
-        end
-      end,
-    }
-  end,
 
   -- This function is run last
   -- good place to configure mappings and vim options
