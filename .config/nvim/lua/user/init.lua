eb42ad2398b134c933c57e563bf96800dad655db
Shantanu Shukla <shantanu.shukla10@gmail.com>
2022-08-07 20:45:21 +0530

    add nvui settings to neovim user config

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -19,13 +19,6 @@ local config = {
 
       -- Add user plugins
       ["lambdalisue/suda.vim"] = {},
-
-      -- Show multiline diagnostics
-      ["https://git.sr.ht/~whynothugo/lsp_lines.nvim"] = {
-        config = function()
-          require("lsp_lines").setup()
-        end,
-      }
     },
 
     -- All other entries override the setup() call for default plugins
@@ -44,9 +37,20 @@ local config = {
 
   -- Diagnostics configuration (for vim.diagnostics.config({}))
   diagnostics = {
-    virtual_text = false,
+    virtual_text = true,
     underline = false,
   },
+
+  -- This function is run last
+  -- good place to configure augroups/autocommands and custom filetypes
+  polish = function()
+    -- Nvui Settings
+    if vim.g.nvui then
+      -- Configure through vim commands
+      vim.cmd [[set guifont=Cascadia\ Code:h11,CaskaydiaCove\ Nerd\ Font]]
+      vim.cmd [[NvuiCursorAnimationDuration 0.1]]
+    end
+  end,
 }
 
 return config
